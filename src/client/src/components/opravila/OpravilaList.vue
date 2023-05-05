be1479546e616c164bbb13292b27e2841faca321
Jan Merhar <merharjan@gmail.com>
2023-05-05 22:26:38 +0200

    Add OpravilaList

---
+++ src/client/src/components/opravila/OpravilaList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import OpraviloCard from "./OpraviloCard.vue"
+</script>
+
+<template>
+  <h3 class="text-center">Opravila</h3>
+  <template v-for="opravilo in opravila" :key="opravilo.id">
+    <opravilo-card :opravilo="opravilo"></opravilo-card>
+  </template>
+</template>
+
+<script>
+export default {
+  components: {
+    OpraviloCard,
+  },
+  props: {
+    opravila: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
