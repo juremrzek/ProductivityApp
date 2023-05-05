fd56d9a324a35a7e3a3a4a9eef480ca4dbce2bcb
Jan Merhar <merharjan@gmail.com>
2023-05-06 01:19:32 +0200

    Add casovnikplayer

---
+++ src/client/src/components/casovnik/CasovnikPlayer.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3 class="text-center">Časovnik</h3>
+  <!-- Odstevanje casa -->
+  <!-- Kontrola casovnika -->
+  <div class="containter">
+    <div class="row box">
+      <div class="col-3"></div>
+      <div class="col">Čas: (se odsteva)</div>
+      <div class="col-1">
+        <button type="button" class="btn btn-secondary">Začni</button>
+      </div>
+      <div class="col-2"></div>
+    </div>
+    <br />
+    <div class="row box">
+      <div class="col">Konec časovnika</div>
+      <div class="col">
+        <button type="button" class="btn btn-secondary">Začni</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-secondary">Začni</button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-secondary">Začni</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+  background-color: rgb(209, 209, 209);
+}
+</style>
