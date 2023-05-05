66934f55f8c22cab0b0e295f81d02d17c5c7301f
Jan Merhar <merharjan@gmail.com>
2023-05-05 21:36:50 +0200

    Remove default css assets

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -4,8 +4,6 @@ import { createPinia } from "pinia"
 import App from "./App.vue"
 import router from "./router"
 
-import "./assets/main.css"
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
