09545ddb37fb0a135f21b2c2564c8d3f27b4bbc4
Jan Merhar <merharjan@gmail.com>
2023-05-06 01:19:45 +0200

    add casovnikview

---
+++ src/client/src/views/CasovnikView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppLayout from "../components/AppLayout.vue"
+import CasovnikPlayer from "../components/casovnik/CasovnikPlayer.vue"
+</script>
+
+<template>
+  <AppLayout> <casovnik-player></casovnik-player> </AppLayout>
+</template>
+
+<script>
+export default {}
+</script>
