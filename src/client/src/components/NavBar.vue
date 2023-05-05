b580730daae60e46fd011cdec44197d4ccee8aa4
Jan Merhar <merharjan@gmail.com>
2023-05-05 17:44:45 +0200

    Add and implement NavBar

---
+++ src/client/src/components/NavBar.vue
@@ -0,0 +1,59 @@
+<script setup></script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand"></a>
+      <button
+        type="button"
+        class="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <!-- Neprijavljen uporabnik -->
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Opravila</a>
+        </div>
+        <!-- Neprijavljen uporabnik -->
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Časovnik</a>
+        </div>
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Koledar</a>
+        </div>
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Iskanje uporabnikov</a>
+        </div>
+        <!-- Neprijavljen uporabnik -->
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Prijavi napako</a>
+        </div>
+        <!-- Admin -->
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Vnesi zaposlenega</a>
+        </div>
+        <!-- Analitik -->
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Prenos podatkov</a>
+        </div>
+        <!-- Razvijalec  -->
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active"
+            >Poročila o nedelovanju</a
+          >
+        </div>
+        <!--  -->
+        <div class="navbar-nav ms-auto">
+          <a href="#" class="nav-item nav-link">Prijava / odjava</a>
+        </div>
+        <!--  -->
+      </div>
+    </div>
+  </nav>
+  <!--  -->
+</template>
+
+<style scoped></style>
