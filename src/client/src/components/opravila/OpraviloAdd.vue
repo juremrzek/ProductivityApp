77bdeb60c2970aa1e2dd252bb6c57c75944de04c
Jan Merhar <merharjan@gmail.com>
2023-05-05 22:24:59 +0200

    Add OpraviloAdd

---
+++ src/client/src/components/opravila/OpraviloAdd.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container form">
+    <h4 class="text-center">Novo opravilo</h4>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Opravilo</label>
+      <input type="email" class="form-control" />
+    </div>
+    <br />
+    <div class="form-group">
+      <label for="exampleFormControlTextarea1">Opis</label>
+      <textarea class="form-control" rows="3"></textarea>
+    </div>
+    <br />
+    <div class="form-group">
+      <label for="exampleInputEmail1">Rok</label>
+      <input
+        type="date"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+    </div>
+    <br />
+    <button type="submit" class="btn btn-secondary">Submit</button>
+  </div>
+</template>
+
+<style scoped>
+.form {
+  margin-top: 0.5rem;
+  padding: 2rem;
+  border: 1px solid black;
+}
+</style>
