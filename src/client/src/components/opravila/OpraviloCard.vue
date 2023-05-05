2c2215a43d52341761740940230a2603483c4b9b
Jan Merhar <merharjan@gmail.com>
2023-05-05 21:59:09 +0200

    Add OpraviloCard.vue

---
+++ src/client/src/components/opravila/OpraviloCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <br />
+  <div class="container-fluid card">
+    <div class="row m-1">
+      <div class="col">Opravilo: {{ opravilo.ime }}</div>
+      <div class="col">Rok: {{ opravilo.rok }}</div>
+      <div class="col-2">
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="this.$event.emit('opravi', opravilo.id)"
+        >
+          Opravi
+        </button>
+      </div>
+    </div>
+
+    <div class="row m-1">
+      <div class="col">Opis: {{ opis }}</div>
+      <div class="col-2">
+        <button
+          type="button"
+          class="btn btn-secondary"
+          @click="this.$event.emit('izbrisi', opravilo.id)"
+        >
+          Izbriši
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OpraviloCard",
+  props: {
+    opravilo: Object,
+  },
+  emits: ["opravi", "izbrisi"],
+}
+</script>
+
+<style scoped>
+.card {
+  margin-top: 0.5rem;
+  border: 1px solid black;
+}
+</style>
