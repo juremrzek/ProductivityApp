d8abd345ddb4a6ab2bc64afb5778d997192c7398
Jan Merhar <merharjan@gmail.com>
2023-05-06 02:12:12 +0200

    add sledilcigrid

---
+++ src/client/src/components/sledilci/SledilciGrid.vue
@@ -0,0 +1,20 @@
+<script setup>
+import SledilecCard from "./SledilecCard.vue"
+</script>
+
+<template>
+  <template v-for="sledilec in sledilci" :key="sledilec.id">
+    <sledilec-card :sledilec="sledilec"></sledilec-card>
+  </template>
+</template>
+
+<script>
+export default {
+  props: {
+    sledilci: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
