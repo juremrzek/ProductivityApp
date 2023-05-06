0f3a460e8d6cb7c6896ec09155018366ce3e4aa4
Jan Merhar <merharjan@gmail.com>
2023-05-06 02:12:21 +0200

    add sledilciview.vue

---
+++ src/client/src/views/SledilciView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AppLayout from "../components/AppLayout.vue"
+import SledilciGrid from "../components/sledilci/SledilciGrid.vue"
+</script>
+
+<template>
+  <app-layout>
+    <sledilci-grid :sledilci="sledilci"></sledilci-grid>
+  </app-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sledilci: [
+        { id: 1, ime: "Janez" },
+        { id: 2, ime: "Micka" },
+        { id: 3, ime: "Miha" },
+      ],
+    }
+  },
+}
+</script>
