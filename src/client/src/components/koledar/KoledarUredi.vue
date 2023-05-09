523046bc41a911c41a0d76e9b1d515b6bd2e6834
Jan Merhar <merharjan@gmail.com>
2023-05-09 16:22:28 +0200

    emitters koledaruredi.vue

--- src/client/src/components/koledar/KoledarUredi.vue
+++ src/client/src/components/koledar/KoledarUredi.vue
@@ -3,33 +3,55 @@
     <h4 class="text-center">Uredi dogodek</h4>
     <div class="form-group">
       <label for="exampleInputEmail1">Ime</label>
-      <input class="form-control" />
+      <input class="form-control" v-model="event.name" />
     </div>
     <br />
     <div class="form-group">
       <label for="exampleInputEmail1">Opis</label>
-      <input class="form-control" />
+      <input class="form-control" v-model="event.description" />
     </div>
     <br />
     <div class="form-group">
       <label for="exampleInputEmail1">Začetek</label>
-      <input type="date" class="form-control" />
+      <input type="date" class="form-control" v-model="event.start" />
     </div>
     <br />
     <div class="form-group">
       <label for="exampleInputEmail1">Trajanje</label>
-      <input type="number" class="form-control" />
+      <input type="number" class="form-control" v-model="event.duration" />
     </div>
     <br />
     <br />
     <br />
     <br />
     <br />
-    <button type="submit" class="btn btn-secondary float-end">Potrdi</button>
+    <button
+      type="submit"
+      class="btn btn-secondary float-end"
+      @click.prevent="$emit('edit', event)"
+    >
+      Potrdi
+    </button>
     <br />
     <br />
   </div>
 </template>
+
+<script>
+export default {
+  name: "dogodekUredi",
+  data() {
+    return {
+      event: {
+        name: "",
+        description: "",
+        start: "",
+        duration: "",
+      },
+    }
+  },
+}
+</script>
 
 <style scoped>
 .form {
