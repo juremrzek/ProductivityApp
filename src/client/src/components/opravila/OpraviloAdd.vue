4f05ab059a26c85a047f9fc959f363c3e19165bc
Jan Merhar <merharjan@gmail.com>
2023-05-09 16:09:56 +0200

    opraviloadd.vue

--- src/client/src/components/opravila/OpraviloAdd.vue
+++ src/client/src/components/opravila/OpraviloAdd.vue
@@ -3,12 +3,16 @@
     <h4 class="text-center">Novo opravilo</h4>
     <div class="form-group">
       <label for="exampleInputEmail1">Opravilo</label>
-      <input type="email" class="form-control" />
+      <input class="form-control" v-model="task.name" />
     </div>
     <br />
     <div class="form-group">
       <label for="exampleFormControlTextarea1">Opis</label>
-      <textarea class="form-control" rows="3"></textarea>
+      <textarea
+        class="form-control"
+        rows="3"
+        v-model="task.description"
+      ></textarea>
     </div>
     <br />
     <div class="form-group">
@@ -19,12 +23,37 @@
         id="exampleInputEmail1"
         aria-describedby="emailHelp"
         placeholder="Enter email"
+        v-model="task.date"
       />
     </div>
     <br />
-    <button type="submit" class="btn btn-secondary">Submit</button>
+    <button
+      type="submit"
+      class="btn btn-secondary float-end"
+      @click.prevent="$emit('dodaj', task)"
+    >
+      Dodaj
+    </button>
+    <br />
+    <br />
   </div>
 </template>
+
+<script>
+export default {
+  name: "OpraviloAdd",
+  data() {
+    return {
+      task: {
+        name: "",
+        description: "",
+        date: "",
+      },
+    }
+  },
+  emits: ["dodaj"],
+}
+</script>
 
 <style scoped>
 .form {
