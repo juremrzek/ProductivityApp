d47c8a4326f8a4d522dc20caf6d965831ded478c
Jan Merhar <merharjan@gmail.com>
2023-05-11 14:46:21 +0200

    add sesarchusers

---
+++ src/client/src/control/SearchUsers.js
@@ -0,0 +1,8 @@
+export class SearchUsers {
+  static async findUsers(name) {}
+  // TODO fix metoda
+  static async getFollowers() {}
+  // TODO fix metoda
+  static async getFollowing() {}
+  static async followUser(id) {}
+}
