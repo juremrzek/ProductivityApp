4552c89716f1b877dd0205ced2fa6bb1b96b7aca
Jan Merhar <merharjan@gmail.com>
2023-05-15 17:47:38 +0200

    add issue

--- src/client/src/control/IssueReport.js
+++ src/client/src/control/IssueReport.js
@@ -1,7 +1,3 @@
 export class IssueReport {
   static async addIssue(issue) {}
-  static async getUnsolvedIssues() {}
-  static async getSolvedIssues(developed) {}
-  static async solveIssue(issue, solvedBy) {}
-  static async rejectIssue(issue) {}
 }
--- src/client/src/views/IssueReportWindow.vue
+++ src/client/src/views/IssueReportWindow.vue
@@ -8,15 +8,25 @@ import WindowManager from "../components/WindowManager.vue"
       <h4 class="text-center">Prijavi napako</h4>
       <div class="form-group">
         <label for="exampleInputEmail1">Ime napake</label>
-        <input type="email" class="form-control" />
+        <input type="email" class="form-control" v-model="name" />
       </div>
       <br />
       <div class="form-group">
         <label for="exampleFormControlTextarea1">Opis napake</label>
-        <textarea class="form-control" rows="3"></textarea>
+        <textarea
+          class="form-control"
+          rows="3"
+          v-model="description"
+        ></textarea>
       </div>
       <br />
-      <button type="submit" class="btn btn-secondary float-end">Prijavi</button>
+      <button
+        type="submit"
+        class="btn btn-secondary float-end"
+        @click="reportIssue()"
+      >
+        Prijavi
+      </button>
       <br />
       <br />
     </div>
@@ -24,7 +34,32 @@ import WindowManager from "../components/WindowManager.vue"
 </template>
 
 <script>
-export default {}
+import { Issue } from "../entities/Issue"
+import { IssueReport } from "../control/IssueReport"
+
+export default {
+  data() {
+    return {
+      name: "",
+      description: "",
+    }
+  },
+  methods: {
+    async reportIssue() {
+      const issue = new Issue(
+        null,
+        this.name,
+        this.description,
+        Date.now(),
+        null,
+        false,
+        null
+      )
+
+      await IssueReport.addIssue(issue)
+    },
+  },
+}
 </script>
 
 <style scoped>
