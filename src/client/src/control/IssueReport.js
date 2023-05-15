bed89a632c23f7795952bb1d95166eccb3fd959a
Jan Merhar <merharjan@gmail.com>
2023-05-15 15:48:28 +0200

    add issuereport.js

--- src/client/src/control/IssueReport.js
+++ src/client/src/control/IssueReport.js
@@ -1,4 +1,7 @@
 export class IssueReport {
   static async addIssue(issue) {}
-  
+  static async getUnsolvedIssues() {}
+  static async getSolvedIssues(developed) {}
+  static async solveIssue(issue, solvedBy) {}
+  static async rejectIssue(issue) {}
 }
