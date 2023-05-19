af0138f6bd759f402d53a21d31bdcc824495ffba
Jan Merhar <merharjan@gmail.com>
2023-05-20 01:28:13 +0200

    add issue report

--- src/client/src/control/IssueReport.js
+++ src/client/src/control/IssueReport.js
@@ -1,3 +1,15 @@
+import { request } from "../utils/request"
+
 export class IssueReport {
-  static async addIssue(issue) {}
+  static async addIssue(issue) {
+    try {
+      const res = await request.post("/addIssue", {
+        title: issue.getTitle(),
+        description: issue.getDescription(),
+      })
+      console.log(res.data)
+    } catch (err) {
+      return null
+    }
+  }
 }
